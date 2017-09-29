16a7e5d816f079b70663afcdae5df70ba9140173
Daniel Till <danieltill@admins-MacBook-Air-10.local>
2017-09-29 12:40:28 -0400

    Initial commit

---
+++ lob/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  lob
+//
+//  Created by Daniel Till on 9/21/17.
+//  Copyright © 2017 Daniel Till. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    // 1
+    let player = SKSpriteNode(imageNamed: "player")
+    
+    override func didMove(to view: SKView) {
+        // 2
+        backgroundColor = SKColor.white
+        // 3
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        // 4
+        addChild(player)
+    }
+}
